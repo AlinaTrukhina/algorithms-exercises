6910782146a4399bfa0a382759eaa117da488ff6
AlinaTrukhina <alina.trukhina@gmail.com>
2023-01-26 11:45:57 -0600

    insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,11 +15,23 @@
 
 function insertionSort(nums) {
   // code goes here
+  // outer loop iterates through the array starting at second number
+  for (let i = 1; i < nums.length; i++) {
+    // inner loop goes backwards and compares the num at index i to all the previous numbers in array
+    for (let j = i; j > 0; j--) {
+      if (Number(nums[j]) < Number(nums[j - 1])) {
+        let temp = nums[j];
+        nums[j] = nums[j-1];
+        nums[j-1] = temp;
+      }
+    }
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
