c45e8973b105560b587826d7401f487cf8716383
AlinaTrukhina <alina.trukhina@gmail.com>
2023-01-26 10:54:12 -0600

    bubble sort try 3

--- specs/bubble-sort/bs.js
+++ specs/bubble-sort/bs.js
@@ -1,15 +1,23 @@
 
 function bubbleSort(nums) {
     // code goes here
-    for(let i = 0; i < nums.length/2; i++) {
-      let earlier = Number(nums[i]);
-      let later = Number(nums[i + 1]);
-      if(earlier > later) {
-        nums[i] = later;
-        nums[i + 1] = earlier;
-      }
-      console.log("i is ", i);
-      console.log(nums);
+    let swaps = 1;
+    // outer loop to go thru the array
+    while (swaps > 0) {
+        swaps = 0;
+        // inner loop to bubble the number up
+        for(let i = 0; i < nums.length - 1; i++) {
+            let earlier = Number(nums[i]);
+            let later = Number(nums[i + 1]);
+            if(earlier > later) {
+              nums[i] = later;
+              nums[i + 1] = earlier;
+              swaps++;
+            }
+            console.log("i is ", i);
+            console.log(swaps);
+            console.log(nums);
+          }
     }
     return nums;
 }
